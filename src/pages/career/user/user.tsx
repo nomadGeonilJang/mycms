c360bbda88b9a3636f5ad5b5c3be841376203d35
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2020-12-21 01:27:58 +0900

    Add User Description

--- src/pages/career/user/user.tsx
+++ src/pages/career/user/user.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { Col, PageHeader, Row } from 'antd';
+import {
+  Col, PageHeader, Row, Descriptions, Typography,
+} from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { RootReducer } from 'pages/career/store';
@@ -21,7 +23,22 @@ function User() {
     <Row justify="center">
       <Col xs={24} md={20} lg={14}>
         <PageHeader title="사용자 정보" onBack={history.goBack}>
-          {user?.name}
+          {user && (
+            <Descriptions layout="vertical" bordered column={1}>
+              <Descriptions.Item label="이름">
+                <Typography.Text>{user.name}</Typography.Text>
+              </Descriptions.Item>
+              <Descriptions.Item label="소속">
+                {user.department}
+              </Descriptions.Item>
+              <Descriptions.Item label="태그">
+                {user.tag}
+              </Descriptions.Item>
+              <Descriptions.Item label="수정내역">
+                수정내역
+              </Descriptions.Item>
+            </Descriptions>
+          )}
         </PageHeader>
       </Col>
     </Row>
